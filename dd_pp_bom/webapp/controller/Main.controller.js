b782ef6cdba8cd583aadcfddc8ea439e59c5daf5
statice447 <star8609@naver.com>
2023-11-26 10:45:30 +0900

    BOM 관리

--- dd_pp_bom/webapp/controller/Main.controller.js
+++ dd_pp_bom/webapp/controller/Main.controller.js
@@ -17,24 +17,31 @@ sap.ui.define(
         onInit: function () {
           this.oRouter = this.getOwnerComponent().getRouter();
           this.oView = this.getView();
-
           var oModel = new JSONModel();
-          var oAddbom = new JSONModel({
-            'Bomid' : '',
-            'Gdcode' : '',
-            'Gdname' : '',
-            'Mtcode' : '',
-            'Delflag' : '',
-            'Crnam' : '',
-            'Erdat' : '',
-            'Ertim' : '',
-            'Chnam' : '',
-            'Chdat' : '',
-            'Chtim' : '',
-            Headertoitem : []
+
+          var oAddBom = new JSONModel({
+              'Gdcode' : '',
+              'Gdname' : '',
+              'Mtcode' : '',
+              BOM_ISet : []
           });
+
+          var oAddbomhead = new JSONModel(
+            {
+              'Gdcode' : '',
+              'Gdname' : '',
+              'Mtcode' : ''
+            }
+          )
+          var oAddbomitem = new JSONModel({
+            Bomitem : []
+          });
+
           this.getView().setModel(oModel, "head");
-          this.getView().setModel(oAddbom,"addTable")
+          this.getView().setModel(oAddBom,"addbom");
+
+          this.getView().setModel(oAddbomitem,"addbomitem");
+          this.getView().setModel(oAddbomhead,"addbomhead");
           this.oRouter.getRoute("Main").attachPatternMatched(this._onPatternMatched, this);
         },
 
@@ -49,13 +56,26 @@ sap.ui.define(
                     return oFormat.format(value);
 
                 }
-            }
+            },
+
+            // fnDelflagToString: function (value) {
+            //   if (value) {
+            //     var oFormat = '가동 중';
+            //     return oFormat.format(value);
+            //   }
+            //   else{
+            //     var oFormat = '가동 중지'
+            //     return oFormat.format(value);
+            //   }
+              
+            // }
         },
   
         onSearch: function () {
           // let oData = this.getView().getModel("head").getData();
-          let oinput = this.byId("BOMidID").getValue();
-          let ocombo = this.byId("comboid").getValue();
+          // let oinput = this.byId("BOMidID").getValue();
+          let oGdname = this.byId("gdnameid").getValue();
+          let oStatus = this.byId("statusid").getValue();
 
           // var oModel = this.getView().getModel("main");
 
@@ -67,13 +87,34 @@ sap.ui.define(
   
           //   debugger;
           let afilter = [];
-          if (oinput) {
-              let oFilter =  new Filter('Bomid', 'Contains', oinput)
-              afilter.push(oFilter);
+          // if (oinput) {
+          //     let oFilter =  new Filter('Bomid', 'Contains', oinput)
+          //     afilter.push(oFilter);
+          // };
+
+          // 기본으로 가동 중인 것만 조회
+          if (!oStatus) {
+            var oFilter =  new Filter('Delflag', 'EQ', false)
+            afilter.push(oFilter);
+          }
+          else{
+            switch(oStatus){
+              case '가동 중':
+                var oFilter =  new Filter('Delflag', 'EQ', false)
+                afilter.push(oFilter);
+                break;
+                
+              case '가동 중지':
+                var oFilter =  new Filter('Delflag', 'EQ', true)
+                afilter.push(oFilter);
+                break;
+            }
+
+            
           };
   
-          if (ocombo) {
-            let oFilter =  new Filter('Gdname', 'Contains', ocombo)
+          if (oGdname) {
+            let oFilter =  new Filter('Gdname', 'Contains', oGdname)
             afilter.push(oFilter);
           };
           // 데이터 바인딩된 정보 경로를 지정(items, rows 등) 테이블 객체를 가져와서, 바인딩 정보를 가져온 후, 필터 적용
@@ -166,99 +207,239 @@ sap.ui.define(
         },
 
         onClose: function(oEvent){
+          let oModel = this.getView().getModel("addbom");
+          let oData = {
+            'Gdcode' : '',
+            'Gdname' : '',
+            'Mtcode' : '',
+            BOM_ISet : []
+          };
+          debugger;
+          // 테이블 리스트 초기화
+          oModel.setProperty("/",oData);
+
+
+          // 콤보 박스 및 인풋 필드 초기화
+          this.byId("idheadGdcode").setValue("");
+          this.byId("idheadGdname").setValue("");
+          this.byId("iditemGdcode").setValue("");
+          this.byId("iditemGdname").setValue("");
+          this.byId("iditemQuan").setValue("");
+
+          this.byId("idheadGdcode").setEditable(true);
+
           oEvent.getSource().getParent().getParent().close(); 
 
         },
         onCreate: function(oEvent){
-          let oMainModel = this.getView().getModel("addTable");
-          let oData = oMainModel.getData();
-              // view에서 바인딩 된 input 값들이 들어온다.
-              /*
-                  들어갈 변수 지정
-                  {
-                      Memid : '입력 값',
-                      Memnm : '입력 값',
-                      Telno : '입력 값',
-                      Email : '입력 값',
-                  }
-              */
-                  debugger;
-              this.getView().getModel().create("/BOM_HSet", oData, {
-                
-                  success : function(oReturn){
-                    debugger;
-                    
-                      MessageToast.show("데이터 생성 완료");
+          // var oHeadModel = this.getView().getModel("addbomhead"),
+          //     oItemModel = this.getView().getModel("addbomitem");
 
-                  },
-                  error : function(oError){
-                    debugger;
-                    MessageToast.show(oError);
-                      console.log(oError)
+          // var oHeadData = oHeadModel.getData(),
+          //     oItemData = oItemModel.getProperty("/Bomitem");
+          var oModel = this.getView().getModel();
+          var oBomModel = this.getView().getModel("addbom");
+          var oBomData = oBomModel.getData();
 
-                  }
+          var oBomData = {
+              "Gdcode" : "P099",
+              "Gdname" : "테스트용",
+              "Mtcode" : "P"
+              // 'BOM_ISet' : []
+              //   {
+              //     "GdcodeM" : "A099",
+              //     "GdnameM" : "테스트용",
+              //     "MtcodeM" : "A",
+              //   }
+              // ]
+          }
 
-              });
+          oModel.create("/BOM_HSet", oBomData, {
+            success : function(oReturn){
+              debugger;
+              MessageToast.show("데이터 생성 완료");
+            },
+
+            error : function(oError){
+              debugger;
+              MessageToast.show(oError);
+              console.log(oError)
+
+            }
+        });
+
+        ////////////////// 두개 모델 따로해서 넣기
+        //   this.getView().getModel().create("/BOM_HSet", oHeadModel.getData(), {
+        //     success : function(oReturn){
+        //       debugger;
+        //       MessageToast.show("데이터 생성 완료");
+        //     },
+
+        //     error : function(oError){
+        //       debugger;
+        //       MessageToast.show(oError);
+        //       console.log(oError)
+
+        //     }
+
+        // });
+  
+          // for(let i = 0; i < oItemModel.getProperty("/Bomitem").length; i++)
+          // {
+          //   this.getView().getModel().create("/BOM_ISet", oItemModel.getProperty("/Bomitem")[i], {
+          //       success : function(oReturn){
+          //         debugger;
+          //         MessageToast.show("데이터 생성 완료");
+          //       },
+
+          //       error : function(oError){
+          //         debugger;
+          //         MessageToast.show(oError);
+          //         console.log(oError)
+
+          //       }
+
+          //   });
+          // }
+          
 
         },
         onDecline: function(oEvent){
-          debugger;
           let getid = oEvent.getSource().getId()
           let index = getid.substr(getid.length -1);
-          let oModel = this.getView().getModel("addTable").getProperty("/Headertoitem");
+          let oData = this.getView().getModel("addbom").getProperty("/BOM_ISet");
 
-          oModel.splice(index, 1);
+          oData.splice(index, 1);
 
-          this.getView().getModel("addTable").refresh(true);
+          this.getView().getModel("addbom").refresh(true);
+
+          if(oData.length == 0){
+            this.byId("idtabletitle").setText("추가된 리스트");
+            this.byId("idheadGdcode").setEditable(true);
+          }
         },
 
         onItemadd: function(oEvent){
+          ////////////// 모델 따로해서 하는 방법 진행중
+          // let iditemgdcode = this.byId("iditemGdcode").getValue(),
+          //     idheadgdcode = this.byId("idheadGdcode").getValue(),
+          //     itemobj = this.getView().getModel().getObject(`/MtmasterSet('${iditemgdcode}')`),
+          //     headobj = this.getView().getModel().getObject(`/MtmasterSet('${idheadgdcode}')`),
+          //     oDatahead = this.getView().getModel("addbomhead").getProperty("/"),
+          //     oDataitem = this.getView().getModel("addbomitem").getProperty("/Bomitem");
+
+
+          // let Bomid = "", GdcodeM = itemobj.Gdcode, GdnameM = itemobj.Gdname, MtcodeM = itemobj.Mtcode, Unit = "EA",
+          //     Quan = this.byId("iditemQuan").getValue();
+
+          // let Gdcode = headobj.Gdcode, Gdname = headobj.Gdname, Mtcode = 'P';
+
+          
+          //   oDatahead.Gdcode = Gdcode;
+          //   oDatahead.Gdname = Gdname;
+          //   oDatahead.Mtcode = Mtcode;
+          //   debugger;
+          
+          // if(oDataitem.length > 0){
+          //   this.byId("idtabletitle").setText(oDatahead.Gdcode + "의 BOM 리스트");
+          // }
+          // else{
+          //   this.byId("idtabletitle").setText("추가된 리스트");
+          // }
+
+          // if(oDataitem.length){
+          //   for(let i = 0; i < oDataitem.length; i++){
+          //     if(oDataitem[i].GdcodeM === itemobj.Gdcode){
+          //       MessageToast.show(iditemgdcode + "는 이미 추가되었습니다.", { width: "15rem" });
+          //       break;
+          //     }
+              
+          //     if(i == (oDataitem.length)-1 && oDataitem[i].GdcodeM !== itemobj.GdcodeM){
+          //       oDataitem.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
+          //       break;
+          //     }
+          //   }
+          // }
+          // else{
+          //   this.byId("idtabletitle").setText(oDatahead.Gdcode + "의 BOM 리스트");
+          //   oDataitem.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
+          // }
+
+          // this.getView().getModel("addbomitem").setProperty("/Bomitem", oDataitem);
+          ///////////////////////////////////////////////////////////////////////////////////////////////
+
           let iditemgdcode = this.byId("iditemGdcode").getValue(),
               idheadgdcode = this.byId("idheadGdcode").getValue(),
               itemobj = this.getView().getModel().getObject(`/MtmasterSet('${iditemgdcode}')`),
               headobj = this.getView().getModel().getObject(`/MtmasterSet('${idheadgdcode}')`),
-              oDatahead = this.getView().getModel("addTable").getProperty("/"),
-              oDatalist = this.getView().getModel("addTable").getProperty("/Headertoitem");
-
-
-          let Bomid = "", GdcodeM = itemobj.Gdcode, GdnameM = itemobj.Gdname, MtcodeM = itemobj.Mtcode, Unit = "EA",
+              oDatahead = this.getView().getModel("addbom").getProperty("/"),
+              oDataitem = this.getView().getModel("addbom").getProperty("/BOM_ISet"),
               Quan = this.byId("iditemQuan").getValue();
 
-          let today = new Date();
-          let Gdcode = headobj.Gdcode, Gdname = headobj.Gdname, Mtcode = 'P', Delflag = "", Crnam = "SNG-19", Erdat = today, Chdat = today;
 
           
+
+          if(idheadgdcode == ""){
+            MessageToast.show("완제품 품목코드를 선택해 주세요");
+          }
+          else if(iditemgdcode == ""){
+            MessageToast.show("원자재 또는 부자재 품목코드를 선택해 주세요");
+          }
+          else if(Quan == ""){
+            MessageToast.show("수량을 입력해 주세요");
+
+          }
+          else{
+            // 헤더 변수 선언
+            let Gdcode = headobj.Gdcode, Gdname = headobj.Gdname, Mtcode = 'P';
+
+            // 아이템 변수 선언
+            let Bomid = "", GdcodeM = itemobj.Gdcode, GdnameM = itemobj.Gdname, MtcodeM = itemobj.Mtcode, Unit = "EA";
+            
+            // 헤더 정보 입력
             oDatahead.Gdcode = Gdcode;
             oDatahead.Gdname = Gdname;
             oDatahead.Mtcode = Mtcode;
-            oDatahead.Crnam = Crnam;
-            oDatahead.Erdat = Erdat;
-            oDatahead.Chdat = Chdat;
             debugger;
           
-          
-          if(oDatalist.length){
-            for(let i = 0; i < oDatalist.length; i++){
-              if(oDatalist[i].GdcodeM === itemobj.Gdcode){
-                MessageToast.show(iditemgdcode + "는 이미 추가되었습니다.", { width: "15rem" });
-                break;
-              }
-              
-              if(i == (oDatalist.length)-1 && oDatalist[i].GdcodeM !== itemobj.GdcodeM){
-                oDatalist.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
-                break;
+            // 테이블 타이틀 체크
+            if(oDataitem.length > 0){
+              this.byId("idtabletitle").setText(oDatahead.Gdcode + "의 BOM 리스트");
+            }
+            else{
+              this.byId("idtabletitle").setText("추가된 리스트");
+            }
+
+            // 중복 품목 검사
+            if(oDataitem.length){
+              for(let i = 0; i < oDataitem.length; i++){
+                if(oDataitem[i].GdcodeM === itemobj.Gdcode){
+                  MessageToast.show(iditemgdcode + "는 이미 추가되었습니다.", { width: "15rem" });
+                  break;
+                }
+                
+                if(i == (oDataitem.length)-1 && oDataitem[i].GdcodeM !== itemobj.GdcodeM){
+                  oDataitem.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
+                  break;
+                }
               }
             }
-          }
-          else{
-            oDatalist.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
+            else{
+              this.byId("idtabletitle").setText(oDatahead.Gdcode + "의 BOM 리스트");
+              oDataitem.push({Bomid, GdcodeM, GdnameM, MtcodeM, Quan, Unit});
+              this.byId("idheadGdcode").setEditable(false);
+
+            }
+
+            // 테이블 세트
+            this.getView().getModel("addbom").setProperty("/BOM_ISet", oDataitem);
           }
 
-          this.getView().getModel("addTable").setProperty("/Headertoitem", oDatalist);
 
-        },
-  
+        }
+      
       });
+    
     }
   );
   
